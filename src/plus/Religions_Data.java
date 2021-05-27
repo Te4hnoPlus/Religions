8f902b7a3e515e81aace58a7c90a271a71a202c3
Te4hnoPlus <79105099+Te4hnoPlus@users.noreply.github.com>
2021-05-27 10:03:26 +0500

    uptime update

--- src/plus/Religions_Data.java
+++ src/plus/Religions_Data.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.bukkit.entity.Player;
 
 public class Religions_Data {
+	
 	private static HashMap<Player, Religions_User> data = new HashMap<>();
 	
 	public static void addUser(Player player) {
@@ -13,14 +14,8 @@ public class Religions_Data {
 			return;
 		}
 		
-		//System.out.println("Добавлен юзер");//
-		
-		//Religions_User user = new Religions_User(player);
-		
 		data.put(player, new Religions_User(player));
-		
-		//System.out.println(data.size());//
-		
+			
 	}
 	
 	public static Religions_User getUser(Player player) {

9a5ae47b371166c35ee5203cf937c6ee4d0ba53b
Te4hnoPlus <79105099+Te4hnoPlus@users.noreply.github.com>
2021-05-27 09:57:18 +0500

    Update Religion_Eat.java

--- src/plus/Religion_Eat.java
+++ src/plus/Religion_Eat.java
@@ -8,18 +8,9 @@ public class Religion_Eat implements Listener{
 	@EventHandler
 	public void opPlayerZgretFood(PlayerItemConsumeEvent e){
 		
-		//System.out.println("тут");
 		Religions_User user = Religions_Data.getUser(e.getPlayer());
 
 		String param = Religions_Core.getPlConfig().getString(user.getReligion()+".food."+e.getItem().getType().name());
-		
-		
-		//System.out.println(param);
-		//System.out.println(user.getReligion()+".food."+e.getItem().getType().name());
-		
-		//Religions_Core.getPlConfig().addDefault(path, value);
-		
-		//System.out.println(param);
 		
 		if(param != null) {
 			Religions_Core.runEffect(e.getPlayer(), param);
